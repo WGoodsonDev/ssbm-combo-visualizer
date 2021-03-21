2f4d18bd473ea6abdf3cfb1089b339d9daa65239
whgoodson <warren.goodson1@gmail.com>
2021-03-20 22:14:20 -0700

    Basic visualization hierarchy, with scaled stage backgrounds and next/prev stage buttons.

--- src/components/sections/VizWindow.js
+++ src/components/sections/VizWindow.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import ControlButton from "../visualizer/control/ControlButton";
+
+import Battlefield from "../../assets/stages/svg/battlefield.svg";
+import Dreamland from "../../assets/stages/svg/dreamland.svg";
+import FinalDestination from "../../assets/stages/svg/FD.svg";
+import FountainOfDreams from "../../assets/stages/svg/fountain.svg";
+import PokemonStadium from "../../assets/stages/svg/stadium.svg";
+import YoshisStory from "../../assets/stages/svg/yoshis.svg";
+
+const stageBackgrounds = [
+    Battlefield,
+    Dreamland,
+    FinalDestination,
+    FountainOfDreams,
+    PokemonStadium,
+    YoshisStory
+];
 
 function VizWindow(props) {
+
+    const [currentStageId, setCurrentStageId] = useState(0);
+
+    const nextStage = () => {
+        if(currentStageId < 5){
+            setCurrentStageId(currentStageId + 1);
+        }
+    }
+
+    const prevStage = () => {
+        if(currentStageId > 0){
+            setCurrentStageId(currentStageId - 1);
+        }
+    }
+
     return (
         <section>
             <div className={"vizOuter"}>
-                <h1>BIG TEST TEXT</h1>
+                <div className={"vizInner"}>
+                    <img alt={"background"} src={stageBackgrounds[currentStageId]}/>
+                    <svg>
+
+                    </svg>
+                </div>
+                <div className={"controlButtons"}>
+                    <ControlButton buttonText={"Next Stage"} onClick={nextStage}/>
+                    <ControlButton buttonText={"Prev Stage"} onClick={prevStage}/>
+                </div>
             </div>
         </section>
     )
---
+++ src/components/visualizer/control/ControlButton.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function ControlButton(props) {
+
+    return (
+        <button type={"button"} value={props.buttonText} onClick={props.onClick}>
+            {props.buttonText}
+        </button>
+    );
+}
+
+export default ControlButton;
