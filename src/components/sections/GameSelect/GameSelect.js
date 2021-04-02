40aa55f6ca59781d41fa3f9ee1ef8491d4eaf3c2
whgoodson <warren.goodson1@gmail.com>
2021-04-01 20:04:22 -0700

    Centered some text/buttons.

--- src/components/sections/GameSelect/GameSelect.js
+++ src/components/sections/GameSelect/GameSelect.js
@@ -4,15 +4,15 @@ function GameSelect(props) {
 
     return (
         <div className={"game-select"}>
+            <p>
+                {props.statusMessage}
+            </p>
             <button className={"get-game-btn"} onClick={props.getGame}>
                 GET RANDOM GAME
             </button>
             <button className={"clear-games-btn"} onClick={props.clearGame}>
                 CLEAR GAME
             </button>
-            <p>
-                {props.statusMessage}
-            </p>
             <div className={"game-card-list"} >
                 {props.children}
             </div>
